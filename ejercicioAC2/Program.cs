4bde69d79cbabe9c069014a97f62673c4c4a5219
cristhian-jesus1 <jesustorresgildres@gmail.com>
2025-10-16 08:58:16 +0200

    subiendo ac2

---
+++ ejercicioAC2/Program.cs
@@ -0,0 +1,114 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //EJERCICIO 1
+
+        // int numero;
+
+        // System.Console.WriteLine("Dime un numero entero");
+        // numero = Convert.ToInt32(Console.ReadLine());
+
+        // if (numero > 0)
+        // {
+        //     System.Console.WriteLine("Numero positivo");
+        // }
+        // if (numero < 0)
+        // {
+        //     System.Console.WriteLine("Numero negativo");
+        // }
+        // if (numero == 0)
+        // {
+        //     System.Console.WriteLine("Numero es cero");
+        // }
+        // System.Console.WriteLine("FIN");
+
+        // EJERCICIO 2
+
+        // int numero1;
+        // int numero2;
+
+        // System.Console.WriteLine("Dime numero1");
+        // numero1 = Convert.ToInt32(Console.ReadLine());
+
+        // System.Console.WriteLine("Dime numero2");
+        // numero2 = Convert.ToInt32(Console.ReadLine());
+
+        // if (numero1 > numero2)
+        // {
+        //     System.Console.WriteLine($"Numero {numero1} es mayor que numero {numero2} ");
+        // }
+        // else if (numero1 < numero2)
+        // {
+        //     System.Console.WriteLine($"Numero {numero2} es mayor que numero {numero1} ");
+        // }
+        // else
+        // {
+        //     System.Console.WriteLine("Los numeros son iguales");
+        // }
+
+        // System.Console.WriteLine("FIN");
+
+        // EJERCICIO 3 
+
+        // int edadUsuario;
+
+        // System.Console.WriteLine("Dime la edad que tienes");
+        // edadUsuario = Convert.ToInt32(Console.ReadLine());
+
+        // if (edadUsuario < 13)
+        // {
+        //     System.Console.WriteLine("Eres un niño");
+        // }
+        // else if ((edadUsuario > 13) && (edadUsuario < 17))
+        // {
+        //     System.Console.WriteLine("Eres un adolescente");
+        // }
+        // else if ((edadUsuario > 18) && (edadUsuario < 64))
+        // {
+        //     System.Console.WriteLine("Eres un adulto");
+        // }
+        // else if (edadUsuario >= 65)
+        // {
+        //     System.Console.WriteLine("Eres mayor");
+        // }
+        // else
+        // {
+        //     System.Console.WriteLine("Estas muerto");
+        // }
+
+        // System.Console.WriteLine("Fin");
+
+        // EJERCICIO 4
+
+        // string usuario;
+        // int password;
+        // System.Console.WriteLine("Introduce el Usuario");
+        // usuario = Console.ReadLine();
+
+        // System.Console.WriteLine("Introduce la password");
+        // password = Convert.ToInt32(Console.ReadLine());
+
+        // if ((usuario == "admin") && (password == 1234))
+        // {
+        //     System.Console.WriteLine("Acceso permitido");
+        // }
+        // else
+        // {
+        //     System.Console.WriteLine("Password Incorrecto o Usuario no reconocido");
+        // }
+
+        // EJERCICIO 5
+
+        // int year;
+
+        // System.Console.WriteLine("Dime un año");
+        // year = Convert.ToInt32(Console.ReadLine());
+
+        // if (year)
+        // {
+            
+        // }
+       
+    }
+}
