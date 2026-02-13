5b9a537b6d1513539d1cbcb475468898f6f39d10
cristhian-jesus1 <jesustorresgildres@gmail.com>
2026-02-13 10:12:47 +0100

    cris

--- segona avaluacio/herencia/herencia 2/ac23/obj/Debug/net8.0/ac23.AssemblyInfo.cs
+++ segona avaluacio/herencia/herencia 2/ac23/obj/Debug/net8.0/ac23.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ac23")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+298bf2ed634dcf5326242ba50515d01ba56e99dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45d6efed9c3a84c9daceafc0852d39d6fb5aae39")]
 [assembly: System.Reflection.AssemblyProductAttribute("ac23")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ac23")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
